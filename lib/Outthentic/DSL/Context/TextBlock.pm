69d3a0e632bc8be64bde23dee7752da16dd63367
Alexey Melezhik <melezhik@gmail.com>
2016-02-10 09:31:44 +0300

    chains!

--- lib/Outthentic/DSL/Context/TextBlock.pm
+++ lib/Outthentic/DSL/Context/TextBlock.pm
@@ -18,15 +18,38 @@ sub change_context {
     #warn Dumper($succ);
 
     my $next_chunk  = [];
-
     if (scalar @{$succ}){
+
+       unless ($self->{chains}){
+            for my $c ( @{$succ} ){
+                $self->{chains}->{$c->[1]} = [$c];
+            }
+       };
+
+       my %keep_chain = map { $_ => 0 } keys %{$self->{chains}};
+         
        for my $c (@{$succ}){
             my $next_i = $c->[1];
-            push @$next_chunk, $orig_ctx->[$next_i]; 
+            push @$next_chunk, $orig_ctx->[$next_i];
+            my $kc;
+            for my $cid (keys %{$self->{chains}}){
+                #warn "a: ".($self->{chains}->{$cid}->[-1]->[1]);
+                #warn "b: ".($next_i-1);
+                if ( $self->{chains}->{$cid}->[-1]->[1] == $next_i-1 ){
+                    $kc = $cid;
+                    $keep_chain{$cid} = 1;
+                    #warn "$c->[0]"
+                }
+            }
+            push @{$self->{chains}->{$kc}}, $c if $kc; 
         }
     }else{
         $next_chunk = $cur_ctx; 
     }
+
+    for my $cid ( keys %keep_chain ){
+        $self->{chains}->{$cid} = undef unless $keep_chain{$cid};
+    }        
     $next_chunk;
 
 }
