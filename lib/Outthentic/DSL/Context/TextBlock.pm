b417d37aec15a0c9f47b18d589245701f47d491d
Alexey Melezhik <melezhik@gmail.com>
2016-12-21 21:04:06 +0300

    stupid bugfix!!!

--- lib/Outthentic/DSL/Context/TextBlock.pm
+++ lib/Outthentic/DSL/Context/TextBlock.pm
@@ -34,6 +34,8 @@ sub change_context {
 
 sub update_stream {
 
+     print "\n\nxxxx start of text block update stream \n \n \n" if $ENV{OUTH_DBG}; 
+
     my $self        = shift;
     my $cur_ctx     = shift; # current search context
     my $orig_ctx    = shift; # original search context
@@ -49,46 +51,59 @@ sub update_stream {
 
        unless ($self->{chains}){ # chain initialization
             for my $c ( @{$succ} ){
-                print "[OTX_DEBUG] init chain $c->[1] ( starting value $c->[0] )...\n" if $ENV{OUTH_DBG};
+                print "[OTX_DEBUG] init chain $c[0]($c[1]) )...\n" if $ENV{OUTH_DBG};
                 $self->{chains}->{$c->[1]} = [$c];
             }
        };
 
 
+
        for my $c (@{$succ}){
-            CHAIN: for my $cid (sort { $a <=> $b } keys %{$self->{chains}}){
+            my @k = keys %{$self->{chains}};
+            CHAIN: for my $cid (sort { $a <=> $b } @k ){
+
                 next CHAIN if $live_chains{$cid};
+
+                print "[OTX_DEBUG] chain end at ".($self->{chains}->{$cid}->[-1]->[1])."\n" if $ENV{OUTH_DBG};
                 
                 if ( $self->{chains}->{$cid}->[-1]->[1] == $c->[1]-1 ){
                   $live_chains{$cid} = 1;
-                  push @{$self-{chains}->{$cid}}, $c;
-                  print "[OTX_DEBUG] push $c->[0] to chain $cid  ...\n" if $ENV{OUTH_DBG};
+                  push @{$self->{chains}->{$cid}}, $c;
+                  print "[OTX_DEBUG] push $c->[0]($c->[1]) to chain $cid  ...\n" if $ENV{OUTH_DBG};
+                  print "[OTX_DEBUG] new chain end at ".($self->{chains}->{$cid}->[-1]->[0])."\n" if $ENV{OUTH_DBG};
                   last CHAIN;
                 }elsif  ( $self->{chains}->{$cid}->[-1]->[1] == $c->[1] ) {
                   $live_chains{$cid} = 1;
                   print "[OTX_DEBUG] keep chain $cid  ...\n" if $ENV{OUTH_DBG};
                   last CHAIN;
-                }
+                }else{
+                  print "[OTX_DEBUG] skip $c->[0] [ $c->[1] ]  ...\n" if $ENV{OUTH_DBG};
+                }  
             }
         }
     }
 
     #warn 100;
     #warn Dumper($succ);
-    #warn Dumper($self->{chains});
+    #print Dumper($self->{chains});
     #warn Dumper([sort { $a <=> $b } keys %live_chains]) if $ENV{OUTH_DBG};
 
     # delete failed chains
 
     ${$stream_ref} = {};
 
+     print "\n\nxxxx generating text block stream xxxx \n \n \n" if $ENV{OUTH_DBG}; 
+
      for my $cid (sort { $a <=> $b } keys %{$self->{chains}}){
-      if (exists $live_chains{$cid}) {        
+      if ($live_chains{$cid}) {        
           ${$stream_ref}->{$cid} = $self->{chains}->{$cid};
+          print "[OTX_DEBUG] chain end at ".($self->{chains}->{$cid}->[-1]->[1])."\n" if $ENV{OUTH_DBG};
       } else {
           delete @{$self->{chains}}{$cid};
       }
     }
+
+     print "\n\nxxxx end of text block update stream \n \n \n" if $ENV{OUTH_DBG}; 
 
 }
 
